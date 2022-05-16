0c9bf19b36440d225ac08c6039f313ea9124d76b
Itz-viju <vijay.web2001@gmail.com>
2022-05-16 18:48:22 +0530

    first commit

---
+++ vijay-test/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'vijay-test';
+  peragraph = 'Welcome to our store';
+}
